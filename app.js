9bd14b8ff721e17bef88461b5a1c6a75b9b8a924
SAMUEL TAI ZI WEI <b022210152@student.utem.edu.my>
2025-01-20 00:21:53 +0800

    stupid changes

--- app.js
+++ app.js
@@ -2207,7 +2207,7 @@ const { message } = require("statuses");
 const Joi = require("joi");
 
 const client = new MongoClient('mongodb+srv://benr2423.jgm92s9.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=BENR2423', {
-  tlsCertificateKeyFile: credentials_testing,
+  tlsCertificateKeyFile: credentials,
   serverApi: ServerApiVersion.v1
 });
 
